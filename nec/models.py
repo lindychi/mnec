b3efac4c50f427d167b2f35084f63047619c993c
hanchi <igam0000@gmail.com>
2017-08-27 13:42:55 +0900

    money unit added

--- nec/models.py
+++ nec/models.py
@@ -5,9 +5,16 @@ from django.utils import timezone
 class BucketList(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=1024)
-    text = models.TextField()
+    text = models.TextField(null=True)
     created_date = models.DateTimeField(default=timezone.now)
-    deadline_date = models.DateTimeField(blank=False, null=False)
-
+    deadline_date = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
+
+class MoneyUnit(models.Model):
+    author = models.ForeignKey('auth.User')
+    category = models.CharField(max_length=1024)
+    title = models.CharField(max_length=1024)
+    text = models.TextField(null=True)
+    balance = models.IntegerField()
+    created_date = models.DateTimeField(blank=False, null=False)
