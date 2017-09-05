414498ee993192707dd3a05e11e0d49cc0766c2a
hanchi <igam0000@gmail.com>
2017-09-06 08:43:53 +0900

    remove bak

--- nec/urls.py
+++ nec/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.dashboard, name='dashboard')
+    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^accounts/login/', auth_views.login, name='login', kwargs={'template_name':'login.html'}),
+    utl(r'^accounts/logout/', auth_views.logout, name='logout', kwargs={'next_page':settings.LOGIN_URL})
 ]
--- nec/views.py
+++ nec/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import *
 
 def dashboard(request):
-    return render(request, 'nec/dashboard.html', {})
+    bucketlists = BucketList.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'nec/dashboard.html', {'bucketlists':bucketlists})
