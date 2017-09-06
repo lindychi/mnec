836b0e388436ca9fde9036f2b42884c3fc7b2807
hanchi <igam0000@gmail.com>
2017-09-06 09:14:30 +0900

    not work fix

--- nec/urls.py
+++ nec/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^accounts/login/', auth_views.login, name='login', kwargs={'template_name':'login.html'}),
-    utl(r'^accounts/logout/', auth_views.logout, name='logout', kwargs={'next_page':settings.LOGIN_URL})
+    url(r'^accounts/login/', auth_views.login, name='login', kwargs={'template_name':'nec/login.html'}),
+    url(r'^accounts/logout/', auth_views.logout, name='logout') #, kwargs={'next_page':settings.LOGIN_URL}
 ]
