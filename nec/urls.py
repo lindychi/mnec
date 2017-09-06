2fbf51ad81cd4b732f6e53aa48eb244bd6881127
hanchi <igam0000@gmail.com>
2017-09-06 20:39:00 +0900

    login logout path update

--- nec/urls.py
+++ nec/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^accounts/login/', auth_views.login, name='login', kwargs={'template_name':'nec/login.html'}),
-    url(r'^accounts/logout/', auth_views.logout, name='logout') #, kwargs={'next_page':settings.LOGIN_URL}
+    url(r'^accounts/logout/', auth_views.logout, name='logout', kwargs={'next_page':settings.LOGIN_URL})
 ]
