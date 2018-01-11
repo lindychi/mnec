17c7735218895d7e7a0ee0b7edad3676ac945c7f
hanchi <igam0000@gmail.com>
2018-01-11 17:50:57 +0900

    wiki url syntax update

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -2,8 +2,10 @@ from django import template
 import re
 
 wikilink = re.compile("\[\[([^\]]+)\]\]")
+wikiurl = re.compile("\[([^|]+)|([^\]]+)\]")
 register = template.Library()
 
 @register.filter
 def wikify(value):
-    return wikilink.sub(r"<a href='/wiki/hanchi/\1/'>\1</a>", value)
+    text = wikilink.sub(r"<a href='/wiki/hanchi/\1/'>\1</a>", value)
+    return wikiurl.sub(r"<a href='\2'>\1</a>", text)
