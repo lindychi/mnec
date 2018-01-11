a66aa6df991909967c74c9cf22c434051ae95bb0
hanchi <igam0000@gmail.com>
2018-01-11 17:54:43 +0900

    wiki url update

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -7,5 +7,5 @@ register = template.Library()
 
 @register.filter
 def wikify(value):
-    text = wikilink.sub(r"<a href='/wiki/hanchi/\1/'>\1</a>", value)
-    return wikiurl.sub(r"<a href='\2'>\1</a>", text)
+    value = wikiurl.sub(r"<a href='\2'>\1</a>", value)
+    return wikilink.sub(r"<a href='/wiki/hanchi/\1/'>\1</a>", value)
