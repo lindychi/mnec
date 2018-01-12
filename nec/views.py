55a5451bc03d2866c892f63044fda67111b3544a
hanchi <igam0000@gmail.com>
2018-01-12 16:37:30 +0900

    link active update

--- nec/views.py
+++ nec/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.conf import settings
 from .models import *
+from nec_todo.models import Todo
 
 def dashboard(request):
     if request.user.is_authenticated:
         bucketlists = BucketList.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
-        todolists = TodoUnit.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+        todolists = Todo.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
         return render(request, 'nec/dashboard.html', {'bucketlists':bucketlists, 'todolists':todolists})
     else:
         return redirect(settings.LOGIN_URL)
