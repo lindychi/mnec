7c7e85739e571523f6069d1865dee7f1cc913f91
hanchi <igam0000@gmail.com>
2018-01-12 17:55:11 +0900

    fix #1 signup update

--- nec/urls.py
+++ nec/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^accounts/login/', auth_views.login, name='login', kwargs={'template_name':'nec/login.html'}),
     url(r'^accounts/logout/', auth_views.logout, name='logout', kwargs={'next_page':settings.LOGIN_URL}),
+    url(r'^accounts/signup/', views.signup, name='signup'),
     url(r'^bank/', views.bank, name='bank'),
 ]
--- nec/views.py
+++ nec/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from django.conf import settings
 from .models import *
+from nec_todo.models import Todo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.urls import reverse
+
 
 def dashboard(request):
     if request.user.is_authenticated:
@@ -11,9 +16,25 @@ def dashboard(request):
     else:
         return redirect(settings.LOGIN_URL)
 
+
 def bank(request):
     if request.user.is_authenticated:
         money_unit_lists = MoneyUnit.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
         return render(request, 'nec/bank.html', {'money_unit_lists':money_unit_lists})
     else:
         return redirect(settings.LOGIN_URL)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(reverse("dashboard"))
+    else:
+        form = UserCreationForm()
+    return render(request, 'nec/signup.html', {'form': form})
