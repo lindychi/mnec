abeed13937e45115a3dcd4cf091f3018871a1285
hanchi <igam0000@gmail.com>
2018-01-17 12:06:51 +0900

    edit delete update

--- nec_todo/views.py
+++ nec_todo/views.py
@@ -33,21 +33,43 @@ def save_new(request, user_name):
         return redirect(settings.LOGIN_URL)
 
 
-def view(request, user_name, todo_name):
+def save(request, user_name, todo_id):
     if request.user.is_authenticated and request.user.username == user_name:
-        todo = Todo.objects.get(owner=request.user, title=todo_name)
-        return render(request, 'nec_todo/view.html', {'user_name':user_name, 'todo':todo})
+        title = request.POST["title"]
+        content = request.POST["content"]
+        try:
+            todo = Todo.objects.get(id=int(todo_id), owner=request.user)
+            todo.content = content
+        except Todo.DoesNotExist:
+            todo = Page(owner=request.user, title=title, content=content)
+        todo.save()
+        return HttpResponseRedirect(reverse('todo_view', args=(user_name, title,)))
     else:
         return redirect(settings.LOGIN_URL)
 
 
-def edit(request, user_name, todo_name):
+def view(request, user_name, todo_name):
     if request.user.is_authenticated and request.user.username == user_name:
-        todo = Todo.objects.get(title=todo_name, owner=request.user)
+        todo_list = Todo.objects.filter(owner=request.user, title=todo_name)
+        return render(request, 'nec_todo/view.html', {'user_name':user_name, 'todo_list':todo_list})
+    else:
+        return redirect(settings.LOGIN_URL)
+
+
+def edit(request, user_name, todo_id):
+    if request.user.is_authenticated and request.user.username == user_name:
+        todo = Todo.objects.get(id=int(todo_id), owner=request.user)
         return render(request, 'nec_todo/edit.html', {'todo':todo})
     else:
         return redirect(settings.LOGIN_URL)
 
 
-def delete(request, user_name, todo_name):
+def delete(request, user_name, todo_id):
+    if request.user.is_authenticated and request.user.username == user_name:
+        todo = Todo.objects.get(id=int(todo_id), owner=request.user)
+        title = todo.title
+        todo.delete()
+        return redirect(reverse('todo_view', args=(request.user.username, title, )))
+    else:
+        return redirect(settings.LOGIN_URL)
     return None
