ea2face0bf951a7249af496479f6b50b9163d80b
hanchi <igam0000@gmail.com>
2018-04-03 15:02:25 +0900

    markdown newline patch

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -9,6 +9,7 @@ register = template.Library()
 @register.filter
 def wikify(value):
     """Substitute the wiki syntax."""
-    value = wikilink.sub(r"<a href='/wiki/page/\1/'>\1</a>", value)
-    value = wikiurl.sub(r"<a href='\2'>\1</a>", value)
-    return value
+    newline_value = re.sub(r"\n", "<br />", value)
+    wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1/'>\1</a>", newline_value)
+    wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", wikilink_value)
+    return wikiurl_value
