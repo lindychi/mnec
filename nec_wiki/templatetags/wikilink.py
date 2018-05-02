6bfef3f0628fb0fdca3a14bb803550db60bb0210
hanchi <igam0000@gmail.com>
2018-05-03 03:58:16 +0900

    wiki other link update

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -1,7 +1,7 @@
 from django import template
 import re
 
-wikilink = re.compile("\[\[([^\]|]+)\]\]")
+wikilink = re.compile("\[\[([^\]]+)\]\]")
 wikiotherlink = re.compile("\[\[([^|]+)|([^\]]+)\]\]")
 wikiurl = re.compile("\[([^|]+)\|([^\]]+)\]")
 youtubelink = re.compile(r"youtube=([0-9a-zA-Z]+)")
@@ -11,8 +11,8 @@ register = template.Library()
 @register.filter
 def wikify(value):
     """Substitute the wiki syntax."""
-    wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", value)
-    youtube_value = youtubelink.sub(r'<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', wikiurl_value)
-    wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1'>\1</a>", youtube_value)
+    wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1'>\1</a>", value)
     wikiotherlink_value = wikiotherlink.sub(r"<a href='/wiki/page/\2'>\1</a>", wikilink_value)
-    return wikiotherlink_value
+    wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", wikiotherlink_value)
+    youtube_value = youtubelink.sub(r'<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', wikiurl_value)
+    return youtube_value
