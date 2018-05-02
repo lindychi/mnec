2ef95cfc0f84031b1e997a8fa263836a40925463
hanchi <igam0000@gmail.com>
2018-05-03 03:48:16 +0900

    link end slash

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -12,5 +12,5 @@ def wikify(value):
     """Substitute the wiki syntax."""
     wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", value)
     youtube_value = youtubelink.sub(r'<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', wikiurl_value)
-    wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1/'>\1</a>", youtube_value)
+    wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1'>\1</a>", youtube_value)
     return wikilink_value
