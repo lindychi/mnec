5c3b5c67dc2cc06e4d48f1224f70a2bd7b6374fb
hanchi <igam0000@gmail.com>
2018-05-03 03:56:07 +0900

    wiki other link update

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -2,6 +2,7 @@ from django import template
 import re
 
 wikilink = re.compile("\[\[([^\]|]+)\]\]")
+wikiotherlink = re.compile("\[\[([^|]+)|([^\]]+)\]\]")
 wikiurl = re.compile("\[([^|]+)\|([^\]]+)\]")
 youtubelink = re.compile(r"youtube=([0-9a-zA-Z]+)")
 register = template.Library()
@@ -13,4 +14,5 @@ def wikify(value):
     wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", value)
     youtube_value = youtubelink.sub(r'<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', wikiurl_value)
     wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1'>\1</a>", youtube_value)
-    return wikilink_value
+    wikiotherlink_value = wikiotherlink.sub(r"<a href='/wiki/page/\2'>\1</a>", wikilink_value)
+    return wikiotherlink_value
