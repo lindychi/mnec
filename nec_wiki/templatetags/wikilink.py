db2668eccefe7f7d5acd88f5073b86c73a0c72cb
hanchi <igam0000@gmail.com>
2018-05-03 04:00:20 +0900

    fix

--- nec_wiki/templatetags/wikilink.py
+++ nec_wiki/templatetags/wikilink.py
@@ -13,6 +13,6 @@ def wikify(value):
     """Substitute the wiki syntax."""
     wikiotherlink_value = wikiotherlink.sub(r"<a href='/wiki/page/\2'>\1</a>", value)
     wikilink_value = wikilink.sub(r"<a href='/wiki/page/\1'>\1</a>", wikiotherlink_value)
-    wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", wikiotherlink_value)
+    wikiurl_value = wikiurl.sub(r"<a href='\2'>\1</a>", wikilink_value)
     youtube_value = youtubelink.sub(r'<iframe width="560" height="315" src="https://www.youtube.com/embed/\1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>', wikiurl_value)
     return youtube_value
